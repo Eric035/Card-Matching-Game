1f470740cf58df7c7d158a3938338e7a61c790a2
Eric035 <ericleung1997@gmail.com>
2019-08-19 09:03:19 -0400

    Added assets, set-up Collection View

---
+++ matchingCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  matchingCards
+//
+//  Created by Eric Leung on 2019-08-19.
+//  Copyright © 2019 Eric Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
